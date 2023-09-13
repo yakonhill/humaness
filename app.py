c52c1b7efdea390811000e477034861da9861d77
= <amitnairlab@gmail.com>
2023-09-13 17:49:08 +0200

    name

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    img_url = None
+    name = None
     if request.method == 'POST':
         name = request.form.get('name')
         if name:
             name = name.capitalize().title()
-            search_term = name + ' square image'
-            url = f"https://www.google.com/images/search?q={search_term}"
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            img_url = soup.find('img')['src']
-    return render_template("index.html", name=img_url)
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run()
